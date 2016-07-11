d3772c3a8c796b4c37722a1df748a90f660506ee
downmooner <11she_232@163.com>
2016-07-11 18:47:44 +0800

    加入信号处理

--- cmd/agent.go
+++ cmd/agent.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"github.com/2qif49lt/agent/pkg/signal"
 	log "github.com/2qif49lt/logrus"
 	"time"
 )
 
 func main() {
-	for i := 0; i < 1000; i++ {
-		log.WithFields(log.Fields{
-			"version":   Version,
-			"buildtime": Buildtime,
-		}).Info("Agent start!")
-		time.Sleep(time.Second / 3)
-	}
+	defer func() {
+		if err := recover(); err != nil {
+			log.Warnln("damn", err)
+			signal.DumpStacks()
+		}
+	}()
+
+	signal.Trap(func() {
+		log.Warnln("oops")
+	})
+
+	log.WithFields(log.Fields{
+		"version":   Version,
+		"buildtime": Buildtime,
+	}).Info("Agent start!")
+
+	time.Sleep(time.Second * 1)
+
 }

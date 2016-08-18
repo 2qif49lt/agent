43514e441fd9ebc3037ff642acd8c2e8375952bf
downmooner <11she_232@163.com>
2016-08-18 18:15:18 +0800

    set random seed

--- main/agent.go
+++ main/agent.go
@@ -5,9 +5,25 @@ import (
 
 	"github.com/2qif49lt/agent/cfg"
 	"github.com/2qif49lt/agent/cli/cmds"
+	"github.com/2qif49lt/agent/utils"
+
+	"math/rand"
+	"os"
+	"time"
 )
 
+func initProgram() {
+	rand.Seed(time.Now().Unix())
+	procdir, _ := utils.GetProcAbsDir()
+	os.Chdir(procdir)
+}
+
+func init() {
+	initProgram()
+}
+
 func main() {
+
 	comflag := cfg.InitCommonFlags()
 	cobraAdaptor := cmds.NewCobraAdaptor(comflag)
 	cobraAdaptor.Cmd().Execute()
